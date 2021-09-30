574d1417b62c260c02ec63a76301de125d22b5a7
DeadUnderscoreMeme <deadunderscorememe@gmail.com>
2021-10-01 04:50:00 +0530

    removed dummy file + pycache

--- meh.py
+++
@@ -1,5 +0,0 @@
-from src.rtml_core import Document
-
-obj = Document.document("./tests/test_files/perfect_file.txt")
-
-print(obj.search_document(search_type=["text", "tag", "subtag"], search_term="lorem"))
